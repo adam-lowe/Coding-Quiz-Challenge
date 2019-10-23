4dea226e3da396a4e56de6198cc68e60bedcf8a7
Goldpm <lowedavidadam@gmail.com>
2019-10-23 17:27:50 -0400

    added array for questions

---
+++ assets/javascript/questions.js
@@ -0,0 +1,18 @@
+var questionsArray = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+      },
+  ];
+  
