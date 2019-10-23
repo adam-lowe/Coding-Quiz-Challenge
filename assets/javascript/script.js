8d7bca8254c2481d565983fd547c86832ef00149
Goldpm <lowedavidadam@gmail.com>
2019-10-23 16:35:52 -0400

    initialized variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,9 @@
 //TODO: Get all the link buttons to function onclick
 //  - get clear highscore button to clear local storage
 // Link this and other js scripts (including jquery) to html
+var timeDisplay = $("#timeDisplay")
+var timer = $("#timer")
+var startBtn = $("#startBtn")
+var timeLeft = 0
+var quizTime = 0
+
