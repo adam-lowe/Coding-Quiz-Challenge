e986b3e4061dd78253c26c990b38aa903973bda9
Goldpm <lowedavidadam@gmail.com>
2019-10-24 16:36:42 -0400

    converted jquery to vanilla

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,20 @@
-var timeDisplay = $("#timeDisplay")
-var timer = $("#timer")
-var startBtn = $("#startBtn")
-var questionDiv = $("#question")
-var answerBtn1 = $("#answer1")
-var answerBtn2 = $("#answer2")
-var answerBtn3 = $("#answer3")
-var answerBtn4 = $("#answer4")
-var feedback = $("#feedback")
+var timeDisplay = document.getElementById("timeDisplay")
+var timer = document.getElementById("timer")
+var startBtn = document.getElementById("startBtn")
+var questionDiv = document.getElementById("question")
+var answerBtn1 = document.getElementById("answer1")
+var answerBtn2 = document.getElementById("answer2")
+var answerBtn3 = document.getElementById("answer3")
+var answerBtn4 = document.getElementById("answer4")
+var feedback = document.getElementById("feedback")
+var title = document.getElementById("pageTitle")
 var qNum = 0
 var timeLeft = 0
 var quizTime = 0
 var score = 0
 
 // this function is to check if current page is highscore page
-if ($('title').html() === "Highscores") {
+if (title.innerHTML === "Highscores") {
     renderTable();
 }
 
@@ -25,14 +26,14 @@ function quizStart() {
 }
 //  function changes timer display every tick (second)
 function startTimer() {
-    timer.html(timeLeft);
+    timer.innerHTML = (timeLeft);
     quizTime = setInterval(tick, 1000);
 }
 //  function equates a tick to a second and determines when timer reaches zero
 function tick() {
     if (timeLeft !== 0) {
      timeLeft--
-     timer.html(timeLeft)
+     timer.innerHTML = (timeLeft)
     }
     else {
         alert("time up")
@@ -42,8 +43,8 @@ function tick() {
 }
 //  function hides initial elements and shows quiz relevant ones, then starts main quiz function
 function initQ() {
-    $(".main").hide();
-    $(".quiz").show();
+    document.querySelectorAll(".main").forEach(main => { main.style.display = "none" })
+    document.querySelectorAll(".quiz").forEach(quiz => { quiz.style.display = "initial" })
     quiz(qNum);
 }
 //  function checks if there are anymore questions and if not ends the quiz
@@ -52,15 +53,15 @@ function quiz() {
     quizOver();
 }
 else {
-    questionDiv.html(questionsArray[qNum].title)
-    answerBtn1.html(questionsArray[qNum].choices[0])
-    answerBtn2.html(questionsArray[qNum].choices[1])
-    answerBtn3.html(questionsArray[qNum].choices[2])
-    answerBtn4.html(questionsArray[qNum].choices[3])
+    questionDiv.innerHTML = (questionsArray[qNum].title)
+    answerBtn1.innerHTML = (questionsArray[qNum].choices[0])
+    answerBtn2.innerHTML = (questionsArray[qNum].choices[1])
+    answerBtn3.innerHTML = (questionsArray[qNum].choices[2])
+    answerBtn4.innerHTML = (questionsArray[qNum].choices[3])
 }}
 //  function checks whether or not answer is the correct one
 function answerCheck(btnId) {
-    if (($("#" + btnId).html()) === (questionsArray[qNum].answer)) {
+    if ((document.getElementById(btnId).innerHTML) === (questionsArray[qNum].answer)) {
         rightAnswer();
         qNum++
     }
@@ -73,40 +74,46 @@ function answerCheck(btnId) {
 //  this function runs when answer is right
 function rightAnswer() {
     score = (score + timeLeft)
-    $(feedback).html("Correct");
-    setTimeout(function() {$(feedback).html("");}, 800)
+    feedback.innerHTML = ("Correct");
+    setTimeout(function() {feedback.innerHTML = ("");}, 800)
 }
 //  this function runs when answer is wrong
 function wrongAnswer() {
     timeLeft = (timeLeft - 15)
-    $(feedback).html("Wrong");
-    setTimeout(function() {$(feedback).html("");}, 800)
+    feedback.innerHTML = ("Wrong");
+    setTimeout(function() {feedback.innerHTML = ("");}, 800)
 }
 
 //  this function generates the end screen and allows user to submit initials with their score
 function quizOver() {
-    $(".quiz").hide();
-    $(".content").prepend('<h1 class="done">All Done!</h1> <button id="submit" class="btn btn-danger">Submit</button> <input id="userScore"> - Enter Initials</input>')
-    $(".done").after('<p id="finalScore">Your final score is ' + score + '</p>');
-    $('#submit').click(function(){
-        var value = $('#userScore').val();
-       localStorage.setItem(value, score);
+    document.querySelectorAll(".quiz").forEach(quiz => { quiz.style.display = "none" })
+    var content = document.getElementById('theContent')
+    var done = document.getElementById("done")
+    var submit = document.getElementById("submit")
+    
+    content.insertAdjacentHTML('afterbegin', '<h1 id="done">All Done!</h1> <button id="submit" class="btn btn-danger">Submit</button> <input id="userScore"> - Enter Initials</input>');
 
-    });  
+    var done = document.getElementById("done")
+    done.insertAdjacentHTML('afterend', '<p id="finalScore">Your final score is ' + score + '</p>');
+
+    var submit = document.getElementById("submit")
+    submit.addEventListener("click", function(){
+        var value = document.getElementById('userScore').value;
+       localStorage.setItem(value, score) });  
     clearInterval(quizTime)
 }
 
 // this function renders the table on the highscore table with the scores from local storage
 function renderTable() {
-    $(".scores").remove();
+    var tbody = document.getElementById("tableBody")
     for (let i = 0; i < localStorage.length; i++) {
      var userName = localStorage.key(i)
      var userScore = localStorage.getItem(userName)
-     $("tbody").after('<tr class="scores"><td>' + userName + ' - ' + userScore + '</td></tr>');
+     tbody.insertAdjacentHTML('afterbegin', '<tr class="scores"><td>' + userName + ' - ' + userScore + '</td></tr>')
     }
 }
 //  this function has the clear highscores button work by clearing local storage and re-rendering table
 function clearStorage() {
     localStorage.clear();
-    renderTable();
+    window.location.reload();
 }
